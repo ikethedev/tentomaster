b17481319c17b0a49461f8884bd507e657bf3c9f
Bruce Sullivan <bruceape@gmail.com>
2024-09-01 09:50:28 -0700

    Fixed some formatting

--- scripts/topbar.js
+++ scripts/topbar.js
@@ -2,30 +2,30 @@
 const TopbarTemplate = document.createElement('template');
 TopbarTemplate.innerHTML = `
 <header class="topbar">
-    <h1 class="topbar__title">My App</h1>
-    <div class="topbar__actions"></div>
+	<h1 class="topbar__title">My App</h1>
+	<div class="topbar__actions"></div>
 </header>
 `
 
 class Topbar {
-    constructor() {
-        // This creates an element to place elements in.
-        // It is based on the template we defined above.
-        this.rootElement = TopbarTemplate.content.cloneNode(true);
-    }
+	constructor() {
+		// This creates an element to place elements in.
+		// It is based on the template we defined above.
+		this.rootElement = TopbarTemplate.content.cloneNode(true);
+	}
 
-    // This method returns the root element of the component.
-    render() {
-        return this.rootElement;
-    }
+	// This method returns the root element of the component.
+	render() {
+		return this.rootElement;
+	}
 
-    // This method sets the title of the topbar.
-    setTitle(title) {
-        this.rootElement.querySelector('.topbar__title').textContent = title;
-    }
+	// This method sets the title of the topbar.
+	setTitle(title) {
+		this.rootElement.querySelector('.topbar__title').textContent = title;
+	}
 
-    // This method adds an action component to the topbar.
-    addAction(action) {
-        this.rootElement.querySelector('.topbar__actions').appendChild(action.render());
-    }
+	// This method adds an action component to the topbar.
+	addAction(action) {
+		this.rootElement.querySelector('.topbar__actions').appendChild(action.render());
+	}
 }
